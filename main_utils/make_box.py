2a789aed68e6c2a0c11374cbbd962767efd4d5d2
JYS <intyeger@gmail.com>
2023-05-20 01:16:53 +0900

    2023/05/20

---
+++ main_utils/make_box.py
@@ -0,0 +1,18 @@
+import os
+import sys
+sys.path.append(os.pardir)
+
+from yolov7.utils.general import scale_coords
+
+
+def convert_coor(img_shape, dets, im0_shape):
+
+    for det in dets:
+
+        if len(det):
+            det[:, :4] = scale_coords(img_shape, det[:, :4], im0_shape).round()
+
+    return dets
+
+
+def draw_box()
